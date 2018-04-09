c4c44570df6c10e856d2c0d0aefaa7241bd0db3b
muhammedts <mohamedts202@gmail.com>
2018-04-09 16:15:25 +0200

    balljumb

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed;
-
+    public float jumpSpeed;
     private Rigidbody rb;
 
     void Start()
@@ -13,13 +13,21 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 atas = new Vector3(0, 20, 0);
+            rb.AddForce(atas * jumpSpeed);
+        }
+        else
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+            rb.AddForce(movement * speed);
+        }
     }
 }
