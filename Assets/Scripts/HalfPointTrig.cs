de867959e6cd9bb025c449d3211f2b1e8ecf928a
mohamedatef96 <Muhamedatef96@gmail.com>
2018-04-25 01:27:46 +0200

    add

--- Assets/Scripts/HalfPointTrig.cs
+++ Assets/Scripts/HalfPointTrig.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HalfPointTargger : MonoBehaviour {
+public class HalfPointTrig : MonoBehaviour {
 
-    // Use this for initialization
     public GameObject LapCompleteTrig;
     public GameObject HalfLapTrig;
+    
     private void OnTriggerEnter(Collider other)
     {
         LapCompleteTrig.SetActive(true);
         HalfLapTrig.SetActive(false);
     }
+
 }
