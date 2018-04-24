47e72a9e4771ce1e2756df153be9fb1fa841f500
mohamedatef96 <Muhamedatef96@gmail.com>
2018-04-24 04:05:44 +0200

    AddHalfPointTrigger

---
+++ Assets/Scripts/HalfPointTargger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfPointTargger : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+    void onTriggerEnter()
+    {
+        LapCompleteTrig.SetActive(true);
+        HalfLapTrig.SetActive(false);
+        
+
+    }
+}
