815206ac13823364a6e19e5f014f14a07fefffb2
mohamedatef96 <Muhamedatef96@gmail.com>
2018-04-27 01:31:28 +0200

    Final

--- Assets/Scripts/LapComplete.cs
+++ Assets/Scripts/LapComplete.cs
@@ -16,57 +16,83 @@ public class LapComplete : MonoBehaviour {
     public GameObject LapTimeBox;
     public GameObject LapCounter;
     public GameObject RaceFinish;
+    public GameObject Player;
+    public GameObject Ai;
 
 
-    public int lapsDone = 0;
+
+
+
+
+    public static int lapsDoneAI;
+    public static int lapsDone;
+
     public float RawTime;
 
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-         
+
+        lapsDone = 0;
+        lapsDoneAI = 0;
+        if (other.gameObject.CompareTag("playercar"))
+        {
+            Debug.Log("playerCar");
             lapsDone += 1;
-        
-        if(LapTimeManager.SecCount <= 9)
-        {
-            SecDisplay.GetComponent<Text>().text = "0" + LapTimeManager.SecCount + ".";
-        } else
-        {
 
-            SecDisplay.GetComponent<Text>().text = "" + LapTimeManager.SecCount + ".";
+            if (LapTimeManager.SecCount <= 9)
+            {
+                SecDisplay.GetComponent<Text>().text = "0" + LapTimeManager.SecCount + ".";
+            }
+            else
+            {
 
-        }
-        if (LapTimeManager.MintCount <= 9)
-        {
-            MinuteDisplay.GetComponent<Text>().text = "0" + LapTimeManager.MintCount + ":";
+                SecDisplay.GetComponent<Text>().text = "" + LapTimeManager.SecCount + ".";
+
+            }
+            if (LapTimeManager.MintCount <= 9)
+            {
+                MinuteDisplay.GetComponent<Text>().text = "0" + LapTimeManager.MintCount + ":";
+            }
+            else
+            {
+
+                MinuteDisplay.GetComponent<Text>().text = "" + LapTimeManager.MintCount + ":";
+
+            }
+            MilliDisplay.GetComponent<Text>().text = "" + LapTimeManager.MilliCount;
+            LapCounter.GetComponent<Text>().text = "" + lapsDone;
+            LapTimeManager.MintCount = 0;
+            LapTimeManager.SecCount = 0;
+            LapTimeManager.MilliCount = 0;
+            HalfLapTrig.SetActive(true);
+            LapCompleteTrig.SetActive(false);
+            
+
         }
         else
         {
+            lapsDoneAI += 1;
+            Debug.Log("AiCar");
 
-            MinuteDisplay.GetComponent<Text>().text = "" + LapTimeManager.MintCount + ":";
 
         }
-        MilliDisplay.GetComponent<Text>().text = "" + LapTimeManager.MilliCount;
-        LapCounter.GetComponent<Text>().text = "" + lapsDone;
-        LapTimeManager.MintCount = 0;
-        LapTimeManager.SecCount = 0;
-        LapTimeManager.MilliCount = 0;
-        HalfLapTrig.SetActive(true);
-        LapCompleteTrig.SetActive(false);
+        
+        if(lapsDone == 2 || lapsDoneAI == 2)
+            {
+            RaceFinish.SetActive(true);
+        }
     }
 
     // Use this for initialization
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(lapsDone == 2)
-        {
-            RaceFinish.SetActive(true);
-        }
-		
+
+
+    // Update is called once per frame
+    void Update () {
+        
 	}
 
 }
--- Assets/Scripts/RaceFinish.cs
+++ Assets/Scripts/RaceFinish.cs
@@ -6,14 +6,29 @@ using UnityStandardAssets.Vehicles.Car;
 public class RaceFinish : MonoBehaviour {
     public GameObject Mycar;
     public GameObject CompleteTrig;
-    private void OnTriggerEnter(Collider other)
-    {
-        Mycar.SetActive(false);
-        CompleteTrig.SetActive(false);
-        CarController.m_Topspeed = 0.0f;
-        Mycar.GetComponent<CarController>().enabled = false;
-        Mycar.GetComponent<CarUserControl>().enabled = false;
 
+    private void OnCollisionEnter(Collision other)
+    {
+        
+            if (other.gameObject.CompareTag("playercar"))
+        {
+            Debug.Log("youWin");
+            Mycar.SetActive(false);
+            CompleteTrig.SetActive(false);
+            CarController.m_Topspeed = 0.0f;
+            Mycar.GetComponent<CarController>().enabled = false;
+            Mycar.GetComponent<CarUserControl>().enabled = false;
+        }
+        else
+        {
+            Debug.Log("Loserrrrrrrrrrrrrrrrr");
+
+            Mycar.SetActive(false);
+            CompleteTrig.SetActive(false);
+            CarController.m_Topspeed = 0.0f;
+            Mycar.GetComponent<CarController>().enabled = false;
+            Mycar.GetComponent<CarUserControl>().enabled = false;
+        }
 
     }
 
