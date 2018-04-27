0202899672707ab32373b126cf6fd32d7bd7a067
mohamedatef96 <Muhamedatef96@gmail.com>
2018-04-27 19:33:40 +0200

    time again

---
+++ Assets/Scripts/StartTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTimer : MonoBehaviour {
+
+    public GameObject Timer;
+    private void OnTriggerEnter(Collider other)
+    {
+        Timer.SetActive(true);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
