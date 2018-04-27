2fc1fe42c547e0c693dd2bc8c24a69cfa2c7c123
Mohamed Ashraf <mohamed.fcih2265@gmail.com>
2018-04-27 18:02:32 +0200

    play again

--- Assets/Scripts/RaceFinish.cs
+++ Assets/Scripts/RaceFinish.cs
@@ -9,6 +9,14 @@ public class RaceFinish : MonoBehaviour
     public GameObject CompleteTrig;
     public GameObject FinishCam;
     public GameObject Cam;
+<<<<<<< HEAD
+    public GameObject end;
+    public GameObject win;
+    public GameObject lose;
+=======
+    public GameObject TimerMange;
+
+>>>>>>> ffa15855b45451b698da002bf880505934776c2b
 
 
 
@@ -30,7 +38,14 @@ public class RaceFinish : MonoBehaviour
             LapTimeManager.MintCount = 0;
             LapTimeManager.SecCount = 0;
             LapTimeManager.MilliCount = 0;
+<<<<<<< HEAD
+            end.SetActive(true);
+            win.SetActive(true);
+=======
+            TimerMange.SetActive(false);
+           
 
+>>>>>>> ffa15855b45451b698da002bf880505934776c2b
 
         }
         else
@@ -45,10 +60,13 @@ public class RaceFinish : MonoBehaviour
             LapTimeManager.MintCount = 0;
             LapTimeManager.SecCount = 0;
             LapTimeManager.MilliCount = 0;
+            TimerMange.SetActive(false);
+
 
             FinishCam.SetActive(true);
             Cam.SetActive(false);
-
+            end.SetActive(true);
+            lose.SetActive(true);
 
 
         }
