4999426c618e8c4297237843020455fe2e82fe14
mohamadomar37 <34622643+mohamadomar37@users.noreply.github.com>
2018-04-28 14:25:30 -0700

    track 1 winner

--- Assets/END Race.cs
+++ Assets/END Race.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class racefinishtrack : MonoBehaviour {
+public class ENDRace : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
