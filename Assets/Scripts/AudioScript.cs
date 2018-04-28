5c8a161d5750ec955322bcbb5e2a8ac8c579bd72
Mohamed Ashraf <mohamed.fcih2265@gmail.com>
2018-04-28 23:05:28 +0200

    music sider

--- Assets/Scripts/AudioScript.cs
+++ Assets/Scripts/AudioScript.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript1 : MonoBehaviour {
-
+public class AudioScript : MonoBehaviour {
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+    public Slider Volume;
 	// Use this for initialization
 	void Start () {
-		
+        MusicSource.clip = MusicClip;
+        MusicSource.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        MusicSource.volume = Volume.value;
 	}
 }
