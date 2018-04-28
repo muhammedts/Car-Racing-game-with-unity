0ad6c4dfe1470da839e9a92a4237265c8e301cfc
mohamedatef96 <Muhamedatef96@gmail.com>
2018-04-28 21:36:30 +0200

    laptimer

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -33,8 +33,8 @@ public class NewBehaviourScript : MonoBehaviour {
             end.SetActive(true);
             win.SetActive(true);
              PlayAgain.SetActive(true);
-            /*TimerMange.SetActive(false);
-            LapTimeManager.MintCount = 0;
+            TimerMange.SetActive(false);
+            /*LapTimeManager.MintCount = 0;
             LapTimeManager.SecCount = 0;
             LapTimeManager.MilliCount = 0;*/
 
@@ -53,8 +53,8 @@ public class NewBehaviourScript : MonoBehaviour {
             end.SetActive(true);
             lose.SetActive(true);
             PlayAgain.SetActive(true);
-            /*  TimerMange.SetActive(false);
-              LapTimeManager.MintCount = 0;
+             TimerMange.SetActive(false);
+              /*LapTimeManager.MintCount = 0;
               LapTimeManager.SecCount = 0;
               LapTimeManager.MilliCount = 0;
               */
