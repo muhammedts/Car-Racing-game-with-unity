438a678bde14aa82c2d75aad30acb89be30f3595
Moaaz2018 <37840227+Moaaz2018@users.noreply.github.com>
2018-04-28 03:26:43 +0200

    second ai

--- Assets/Scripts/CarControlActive.cs
+++ Assets/Scripts/CarControlActive.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 
 public class CarControlActive : MonoBehaviour
 {
    public GameObject CarControl;
     public GameObject AIControl;
+    public GameObject AIControl2;
     void Start()
     {
         (CarControl.GetComponent("CarController") as MonoBehaviour).enabled = true;
         (CarControl.GetComponent("CarUserControl") as MonoBehaviour).enabled = true;
         (AIControl.GetComponent("CarAIControl") as MonoBehaviour).enabled = true;
         (AIControl.GetComponent("CarController") as MonoBehaviour).enabled = true;
+        (AIControl2.GetComponent("CarAIControl") as MonoBehaviour).enabled = true;
+        (AIControl2.GetComponent("CarController") as MonoBehaviour).enabled = true;
+
 
 
 
